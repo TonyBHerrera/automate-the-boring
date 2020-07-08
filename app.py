410daede6400a567b9c115369a2740c7aa10f087
TonyBHerrera <tonybherrera@yahoo.com>
2020-07-08 11:45:14 -0600

    going through some loop practice and other basics

---
+++ app.py
@@ -0,0 +1,46 @@
+# this program says hello and asks for my name
+
+# print("hello world!")
+# print("What is your name?")
+# myName = input()
+# print("its good to me you, " + myName)
+# print("the length of your name is:")
+# print(len(myName))
+# print('whats your age?')
+# myAge = input()
+# print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+# name = 'Alice'
+# if name == 'Alice':
+#     print('Hi Alice')
+# print('done')    
+
+
+# password = "swordfish"
+# if password == 'swordfish':
+#     print("access granted")
+# else: 
+#     print("wrong password")    
+
+# name = "bob"
+# age = 3000
+# if name == "Alice":
+#     print("hi Alice")
+# elif age < 12:
+#     print('your not alice kiddo')
+# elif age > 2000:
+#     print("Unlike you alice is not an undead vampire")
+# elif age > 100:
+#     print('your not alice, grannie')
+
+# spam = 0 
+# while spam < 5:
+#     print("hello world")
+#     spam = spam + 1
+
+name = ''
+
+while name != "your name":
+    print("Please type your name")
+    name = input()
+print("thank you")    
